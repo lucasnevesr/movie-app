e9ede3103fe3de6b60eff072069edf0414e94161
Lucas Neves <nevessricardoo@gmail.com>
2024-10-03 20:31:13 -0300

    Booted a new feature in SearchInput for search a movie for the Title

--- src/SearchInput/SearchInput.jsx
+++ src/SearchInput/SearchInput.jsx
@@ -1,12 +1,29 @@
 import "./SearchInput.css";
-
+import { useState, useEffect } from "react";
 
 function SearchInput() {
+
+const [inputValue, setInputValue] = useState('')
+// const url = `https://api.themoviedb.org/3/search/movie?query=${}`
+
+function handleChange(event){
+  setInputValue(event.target.value)
+};
+
+useEffect(() => {
+
+if (inputValue) {
+  return console.log(inputValue);
+}
+
+}, [inputValue]);
+
+
   return (
     <div className="searchbar">
       <div className="searchbarContent">
          <div className="searchIcon"/>
-          <input type="text" placeholder="Search" className="searchbarInput" />
+          <input onChange={handleChange} input={inputValue} type="text" placeholder="Search" className="searchbarInput" />
          
       </div>
     </div>
